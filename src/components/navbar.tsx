4e5c7973d21910b0bd67cae696ee63308dcbe5fa
Saad Tariq <saadtariq.dev@outlook.com>
2023-12-31 03:56:39 -0500

    feat: create navbar component for dashboard

---
+++ src/components/navbar.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from "@clerk/nextjs"
+import { Logo } from "./logo"
+import { ThemeSwitcher } from "./theme-switcher"
+
+export function Navbar() {
+  return (
+    <nav className="flex h-16 items-center justify-between border-b border-border px-4 py-2">
+      <Logo />
+      <section className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <UserButton afterSignOutUrl="/sign-in" />
+      </section>
+    </nav>
+  )
+}
