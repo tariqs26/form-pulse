271978485fb55d5ee464ccbd52aaa51e50f6b383
Saad Tariq <saadtariq.dev@outlook.com>
2023-12-31 05:05:09 -0500

    refactor update import statement for DropdownMenuPrimitive in dropdown-menu.tsx

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { forwardRef } from "react"
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
+import DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
 import {
   CheckIcon,
   ChevronRightIcon,
