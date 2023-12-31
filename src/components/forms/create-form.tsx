0f03a9a31e04d5076842204a4e79d00591c34bfd
Saad Tariq <saadtariq.dev@outlook.com>
2023-12-31 07:04:51 -0500

    feat: add form to create forms given name and description

---
+++ src/components/forms/create-form.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { formSchema, FormData } from "@/schemas/form"
+import { createForm } from "@/actions/form"
+import { catchAsync } from "@/lib/utils"
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "../ui/input"
+import { Textarea } from "../ui/textarea"
+import { Button } from "../ui/button"
+import { Loader } from "lucide-react"
+import { toast } from "@/hooks/use-toast"
+
+export function CreateForm() {
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  })
+
+  async function onSubmit(data: FormData) {
+    const res = await catchAsync(createForm(data), "Form")
+
+    if ("error" in res)
+      toast({
+        title: "Error",
+        description: res.error,
+        variant: "destructive",
+      })
+    else toast(res)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea rows={3} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={form.formState.isSubmitting}>
+          {form.formState.isSubmitting ? (
+            <Loader className="animate-spin" />
+          ) : (
+            "Save"
+          )}
+        </Button>
+      </form>
+    </Form>
+  )
+}
