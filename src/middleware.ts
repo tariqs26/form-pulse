a7cc2f39ae282b546600f98768a8b4364f134543
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-01 07:27:33 -0500

    refactor: add redirect from landing page to dashboard when logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req, _) {
+    // redirect from landing page to dashboard when logged in
+    if (auth.userId && req.nextUrl.pathname === "/") {
+      const url = new URL("/dashboard", req.url)
+      return NextResponse.redirect(url)
+    }
+    return NextResponse.next()
+  },
 })
 
 export const config = {
