4422b2d0aacd88f8b648212b536c24e75a9399c5
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-03 17:19:29 -0500

    feat: add NextTopLoader component to layout to show loading progress when routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
+import NextTopLoader from "nextjs-toploader"
 import { ThemeProvider } from "@/components/providers/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { siteMetadata } from "@/lib/seo"
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: LayoutProps) {
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <NextTopLoader />
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
