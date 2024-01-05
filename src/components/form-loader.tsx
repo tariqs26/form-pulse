3173a52c302fc744b4d96d26cdfc77df7ddf9247
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-04 23:26:55 -0500

    feat: add component for form loading page

---
+++ src/components/form-loader.tsx
@@ -0,0 +1,9 @@
+import { Spinner } from "@/components/ui/spinner"
+
+export function FormLoader() {
+  return (
+    <div className="flex flex-grow items-center justify-center">
+      <Spinner className="h-12 w-12" />
+    </div>
+  )
+}
