fea7bde4e4632f3e3dd8e438281f675bb0fb97fb
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-05 05:13:31 -0500

    refactor: re-order actions, add submit form, get form by shareId and get form with submissions actions

--- src/actions/form.ts
+++ src/actions/form.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { revalidatePath } from "next/cache"
 import db from "@/lib/db"
 import { FormData } from "@/schemas/form"
 import { currentUser } from "@clerk/nextjs"
@@ -11,6 +12,22 @@ async function getUserOrThrow() {
   return user
 }
 
+export async function createForm(data: FormData) {
+  const user = await getUserOrThrow()
+
+  const form = await db.form.create({
+    data: {
+      ...data,
+      userId: user.id,
+    },
+  })
+
+  return {
+    id: form.id,
+    title: "Success",
+    description: "Form created successfully",
+  }
+}
 export async function getUserFormStats() {
   const user = await getUserOrThrow()
 
@@ -35,23 +52,6 @@ export async function getUserFormStats() {
     submissions,
     submissionRate,
     bounceRate,
-  }
-}
-
-export async function createForm(data: FormData) {
-  const user = await getUserOrThrow()
-
-  const form = await db.form.create({
-    data: {
-      ...data,
-      userId: user.id,
-    },
-  })
-
-  return {
-    id: form.id,
-    title: "Success",
-    description: "Form created successfully",
   }
 }
 
@@ -80,6 +80,33 @@ export async function getFormById(id: number) {
   return form
 }
 
+export async function getFormContentByShareId(shareId: string) {
+  const form = await db.form.update({
+    where: { shareId },
+    select: {
+      content: true,
+    },
+    data: {
+      visits: {
+        increment: 1,
+      },
+    },
+  })
+
+  return form.content as FormElementInstance[]
+}
+
+export async function getFormWithSubmissions(id: number) {
+  const user = await getUserOrThrow()
+  const form = await db.form.findUniqueOrThrow({
+    where: { id, userId: user.id },
+    include: {
+      formSubmissions: true,
+    },
+  })
+  return form
+}
+
 export async function updateFormContent(
   id: number,
   content: FormElementInstance[],
@@ -88,10 +115,10 @@ export async function updateFormContent(
 
   const form = await db.form.update({
     where: { id, userId: user.id },
-    data: {
-      content,
-    },
+    data: { content },
   })
+
+  revalidatePath(`/dashboard/builder/${id}`)
 
   return form
 }
@@ -101,13 +128,27 @@ export async function publishForm(id: number) {
 
   const form = await db.form.update({
     where: { id, userId: user.id },
-    data: {
-      published: true,
-    },
+    data: { published: true },
   })
 
   return form
+}
 
+export async function submitForm(
+  shareId: string,
+  content: Record<string, any>,
+) {
+  const submission = await db.form.update({
+    where: { shareId, published: true },
+    data: {
+      submissions: { increment: 1 },
+      formSubmissions: {
+        create: { content },
+      },
+    },
+  })
+
+  return submission
 }
 
 export async function deleteForm(id: number) {
