6979aad026ac1758ad44fceee8a1fc27d431d36e
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-06 15:17:33 -0500

    fix: middleware logic for redirecting signed in users to dashboard page

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,20 @@ import { NextResponse } from "next/server"
 export default authMiddleware({
   publicRoutes: ["/", "/submit"],
   afterAuth(auth, req, _) {
-    if (auth.userId === null && req.nextUrl.pathname.startsWith("/dashboard")) {
+    // Redirect to dashboard if user is signed in
+
+    const isAuthRoute =
+      req.nextUrl.pathname.startsWith("/sign-in") ||
+      req.nextUrl.pathname.startsWith("/sign-up")
+
+    if (auth.userId !== null && isAuthRoute) {
+      const url = new URL("/dashboard", req.url)
+      return NextResponse.redirect(url)
+    }
+
+    const isProtectedRoute = req.nextUrl.pathname.startsWith("/dashboard")
+
+    if (auth.userId === null && isProtectedRoute) {
       const url = new URL("/sign-in", req.url)
       url.searchParams.set("redirect_url", req.nextUrl.pathname)
       return NextResponse.redirect(url)
