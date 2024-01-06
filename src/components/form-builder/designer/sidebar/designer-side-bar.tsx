19893fab5330a32007455c98d4afab4f78d31e72
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-06 16:47:24 -0500

    fix: designer sidebar width

--- src/components/form-builder/designer/sidebar/designer-side-bar.tsx
+++ src/components/form-builder/designer/sidebar/designer-side-bar.tsx
@@ -5,7 +5,7 @@ import { FormElementsSidebar } from "./form-elements-sidebar"
 export function DesignerSideBar() {
   const { selectedElement } = useDesigner()
   return (
-    <aside className="flex self-stretch flex-col md:min-w-fit overflow-y-auto border-l-2 border-muted bg-background p-4">
+    <aside className="flex self-stretch flex-col w-full max-w-52 md:max-w-72 overflow-y-auto border-l-2 border-muted bg-background p-4">
       {selectedElement ? <FormPropertiesSidebar /> : <FormElementsSidebar />}
     </aside>
   )
