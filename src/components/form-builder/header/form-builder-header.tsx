7b24ffbff13264838dd0665ecf79004261557272
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-06 16:18:44 -0500

    style: format

--- src/components/form-builder/header/form-builder-header.tsx
+++ src/components/form-builder/header/form-builder-header.tsx
@@ -10,7 +10,7 @@ export function FormBuilderHeader(form: Form) {
         Form:
         <span className="ml-2 text-muted-foreground">{form.name}</span>
       </h4>
-      <div className="flex items-center gap-2 flex-wrap">
+      <div className="flex flex-wrap items-center gap-2">
         <PreviewDialogButton />
         {!form.published && (
           <>
