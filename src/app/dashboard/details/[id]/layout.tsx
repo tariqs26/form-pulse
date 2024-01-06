cf2bd6c6ccd847523cc1b9d71b8bf9af86a8c149
Saad Tariq <saadtariq.dev@outlook.com>
2024-01-06 16:20:45 -0500

    refactor: add flex-grow to layout styling

--- src/app/dashboard/details/[id]/layout.tsx
+++ src/app/dashboard/details/[id]/layout.tsx
@@ -1,6 +1,6 @@
 export default function DetailsLayout({ children }: LayoutProps) {
   return (
-    <div className="mx-auto flex flex-col overflow-x-hidden">
+    <div className="flex flex-col overflow-x-hidden flex-grow">
       {children}
     </div>
   )
