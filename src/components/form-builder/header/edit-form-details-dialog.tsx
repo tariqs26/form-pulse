0d7c9dd47aa3d4a63f36596d9c66014e463dc46b
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-01 18:25:28 -0400

    refactor: add link to navigate back to dashboard and add dialog to edit form details

---
+++ src/components/form-builder/header/edit-form-details-dialog.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState } from "react"
+import type { Form } from "@prisma/client"
+import { Edit } from "lucide-react"
+
+import { DetailsForm } from "@/components/forms/details-form"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+export const EditFormDetailsDialog = (form: Readonly<Form>) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="secondary">
+          <Edit size={16} className="mr-2" />
+          Edit Details
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Form Details</DialogTitle>
+        </DialogHeader>
+        <DetailsForm
+          defaultValues={form}
+          closeModal={() => {
+            setOpen(false)
+          }}
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
--- src/components/form-builder/header/form-builder-header.tsx
+++ src/components/form-builder/header/form-builder-header.tsx
@@ -1,14 +1,23 @@
+import Link from "next/link"
 import type { Form } from "@prisma/client"
+import { ChevronRight } from "lucide-react"
+
 import { PreviewDialogButton } from "./preview-dialog-button"
 import { SaveFormButton } from "./save-form-button"
 import { PublishFormButton } from "./publish-form-button"
+import { EditFormDetailsDialog } from "./edit-form-details-dialog"
 
 export const FormBuilderHeader = (form: Readonly<Form>) => (
   <header className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 border-b-2 p-4">
-    <h4 className="truncate font-medium">
-      Form:<span className="ml-2 text-muted-foreground">{form.name}</span>
-    </h4>
+    <div className="flex items-center truncate text-lg font-medium text-muted-foreground">
+      <Link href="/dashboard" className="text-foreground hover:underline">
+        Forms
+      </Link>
+      <ChevronRight size={20} />
+      {form.name}
+    </div>
     <div className="flex flex-wrap items-center gap-2">
+      {!form.published && <EditFormDetailsDialog {...form} />}
       <PreviewDialogButton />
       {!form.published && (
         <>
