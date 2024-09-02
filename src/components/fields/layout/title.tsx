9bfe618eaa1549ecdc13c34b8747726a98b236a7
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-02 00:38:37 -0400

    style: remove unused import

--- src/components/fields/layout/title.tsx
+++ src/components/fields/layout/title.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useEffect } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Heading1 } from "lucide-react"
