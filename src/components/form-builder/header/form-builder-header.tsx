ca2b3bafab979518da4c8b837b279be406bca026
Saad Tariq <saadtariq.dev@outlook.com>
2024-09-05 11:56:10 -0400

    refactor: update form preview dialog to handle submission previews

--- src/components/form-builder/header/form-builder-header.tsx
+++ src/components/form-builder/header/form-builder-header.tsx
@@ -2,29 +2,34 @@ import Link from "next/link"
 import type { Form } from "@prisma/client"
 import { ChevronRight } from "lucide-react"
 
-import { PreviewDialogButton } from "./preview-dialog-button"
+import { useDesigner } from "@/hooks/use-designer"
+import { FormPreviewDialog } from "@/components/form-preview-dialog"
 import { SaveFormButton } from "./save-form-button"
 import { PublishFormButton } from "./publish-form-button"
 import { EditFormDetailsDialog } from "./edit-form-details-dialog"
 
-export const FormBuilderHeader = (form: Readonly<Form>) => (
-  <header className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 border-b-2 p-4">
-    <div className="flex items-center truncate text-lg font-medium text-muted-foreground">
-      <Link href="/dashboard" className="text-foreground hover:underline">
-        Dashboard
-      </Link>
-      <ChevronRight size={20} />
-      {form.name}
-    </div>
-    <div className="flex flex-wrap items-center gap-2">
-      {form.status === "DRAFT" && <EditFormDetailsDialog {...form} />}
-      <PreviewDialogButton />
-      {form.status === "DRAFT" && (
-        <>
-          <SaveFormButton {...form} />
-          <PublishFormButton {...form} />
-        </>
-      )}
-    </div>
-  </header>
-)
+export const FormBuilderHeader = (form: Readonly<Form>) => {
+  const { elements } = useDesigner()
+
+  return (
+    <header className="flex flex-wrap items-center justify-between gap-x-6 gap-y-4 border-b-2 p-4">
+      <div className="flex items-center truncate text-lg font-medium text-muted-foreground">
+        <Link href="/dashboard" className="text-foreground hover:underline">
+          Dashboard
+        </Link>
+        <ChevronRight size={20} />
+        {form.name}
+      </div>
+      <div className="flex flex-wrap items-center gap-2">
+        {form.status === "DRAFT" && <EditFormDetailsDialog {...form} />}
+        <FormPreviewDialog elements={elements} />
+        {form.status === "DRAFT" && (
+          <>
+            <SaveFormButton {...form} />
+            <PublishFormButton {...form} />
+          </>
+        )}
+      </div>
+    </header>
+  )
+}
---
+++ src/components/form-preview-dialog.tsx
@@ -0,0 +1,69 @@
+import { ScanEye } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import {
+  formElements,
+  type FormElementInstance,
+  type UserFormSubmission,
+} from "@/types/form-builder"
+
+import { FormPreviewContainer } from "@/components/form-preview-container"
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+
+export type FormPreviewDialogProps = Readonly<{
+  elements: FormElementInstance[]
+  submission?: UserFormSubmission
+}>
+
+export const FormPreviewDialog = ({
+  elements,
+  submission,
+}: FormPreviewDialogProps) => {
+  const hasSubmission = submission !== undefined
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className={cn("gap-2", hasSubmission && "size-8")}
+          size={hasSubmission ? "icon" : "default"}
+          variant={hasSubmission ? "secondary" : "outline"}
+        >
+          <ScanEye size={hasSubmission ? 20 : 16} />
+          {!hasSubmission && "Preview"}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="flex h-full max-h-screen max-w-full flex-grow flex-col gap-0 p-0">
+        <header className="border-b px-4 py-2">
+          <p className="text-lg font-bold">
+            Form {hasSubmission && "Submission"} Preview
+          </p>
+          <p className="text-sm text-muted-foreground">
+            {hasSubmission
+              ? "This is how the form looked like when it was submitted."
+              : "This is how your form will look like to users."}
+          </p>
+        </header>
+        <section className="bg-graph flex flex-grow items-center justify-center overflow-y-auto p-4">
+          <FormPreviewContainer>
+            {elements.map((element) => {
+              const FormComponent = formElements[element.type].formComponent
+
+              return (
+                <FormComponent
+                  key={element.id}
+                  elementInstance={element}
+                  defaultValue={
+                    hasSubmission ? submission.content[element.id] : undefined
+                  }
+                  disabled={hasSubmission}
+                />
+              )
+            })}
+          </FormPreviewContainer>
+        </section>
+      </DialogContent>
+    </Dialog>
+  )
+}
