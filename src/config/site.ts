106fe3d0cba65394575eb1472ba2d82fa66fe6a2
Saad Tariq <saadtariq.dev@outlook.com>
2024-12-19 16:27:57 -0500

    refactor: add metadataBase to site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,8 @@
+import type { Metadata } from "next"
+
 export const siteConfig = {
   title: "FormPulse",
   description:
     "Form Pulse is an application that allows users to create and share forms with others.",
-}
+  metadataBase: new URL("https://formpulse.vercel.app"),
+} satisfies Metadata
