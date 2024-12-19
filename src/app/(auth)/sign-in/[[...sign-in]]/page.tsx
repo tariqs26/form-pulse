25ee50693be36e69de2d1094dc3dd62c5fff01dd
Saad Tariq <saadtariq.dev@outlook.com>
2024-12-19 16:28:09 -0500

    refactor: add title for Sign In and Sign Up pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs"
 
+export const metadata = {
+  title: "Sign In",
+}
+
 export default function SignInPage() {
   return <SignIn />
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs"
 
+export const metadata = {
+  title: "Sign Up",
+}
+
 export default function SignUpPage() {
   return <SignUp />
 }
