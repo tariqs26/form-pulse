a6ebc5cd658bf8a7e98542fa8711421d1446a406
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-08 15:28:55 -0400

    refactor: simplify sensor configuration in useFormBuilderSensors hook

--- src/hooks/use-form-builder-sensors.ts
+++ src/hooks/use-form-builder-sensors.ts
@@ -1,31 +1,14 @@
-import {
-  MouseSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-  type DndContextProps,
-} from "@dnd-kit/core"
+import { MouseSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core"
 
 export const useFormBuilderSensors = () => {
   const mouseSensor = useSensor(MouseSensor, {
-    activationConstraint: {
-      distance: 10, // 10px
-    },
+    activationConstraint: { distance: 10 }, // 10px
   })
 
   // mobile devices
   const touchSensor = useSensor(TouchSensor, {
-    activationConstraint: {
-      delay: 300, // 300ms
-      tolerance: 5, // 5px
-    },
+    activationConstraint: { delay: 300, tolerance: 5 }, // 300ms, 5px
   })
 
-  // Allows delete button and properties trigger to be clicked, because the drag activation constraint distance is 10px
-  const sensors: DndContextProps["sensors"] = useSensors(
-    mouseSensor,
-    touchSensor
-  )
-
-  return sensors
+  return useSensors(mouseSensor, touchSensor)
 }
