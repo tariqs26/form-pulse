ab28028aceacfffeb2bca8b94c5bd07b3ee30058
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-08 15:47:28 -0400

    refactor: add eslint directive for global prisma variable declaration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from "@prisma/client"
 const prismaClientSingleton = () => new PrismaClient()
 
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
 }
 
