30053965e04bbab45f41321b897ac12a4f79b98e
Saad Tariq <saadtariq.dev@outlook.com>
2025-06-09 15:25:04 -0400

    refactor: rename route parameter from `formShareId` to `shareId`

--- src/app/submit/[shareId]/page.tsx
+++ src/app/submit/[shareId]/page.tsx
@@ -2,16 +2,14 @@ import { getFormContentByShareId } from "@/actions/form"
 import { FormError } from "@/components/form-error"
 import { FormSubmit } from "@/components/submit/form-submit"
 
-type Props = Readonly<{ params: { formShareId: string } }>
-
 const link = { href: "/", text: "Back to home" }
 
-export default async function SubmitPage({ params }: Props) {
-  const formContent = await getFormContentByShareId(params.formShareId)
+export default async function SubmitPage({
+  params,
+}: Readonly<{ params: { shareId: string } }>) {
+  const formContent = await getFormContentByShareId(params.shareId)
 
   if ("error" in formContent) return <FormError link={link} {...formContent} />
 
-  return (
-    <FormSubmit formContent={formContent} formShareId={params.formShareId} />
-  )
+  return <FormSubmit formContent={formContent} formShareId={params.shareId} />
 }
