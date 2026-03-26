33ef57d531c9a021bf0a853161fd32d4b4d44e23
Saad Tariq <saadtariq.dev@outlook.com>
2026-03-25 22:09:49 -0400

    refactor: add type for middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+import type { MiddlewareConfig } from "next/server"
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
@@ -6,7 +7,7 @@ export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
 })
 
-export const config = {
+export const config: MiddlewareConfig = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
